9f316c0669bae7cbf961eb6bcf5c0a4448d3dc18
IMRANDIL <aliimranadil2@gmail.com>
2023-08-13 09:46:09 +0530

    added function to index the product model

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,3 +1,13 @@
+const { Client } = require("@elastic/elasticsearch");
+const client = new Client({ node: "http://localhost:9200" });
+
+async function indexProduct(product) {
+  await client.index({
+    index: "products",
+    body: product,
+  });
+}
+
 const createProduct = async (req, res, next) => {};
 
 const searchProduct = async (req, res, next) => {};
